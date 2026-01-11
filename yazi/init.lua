69912541f9c49af1f2c3a401b91e4f9bb42833a2
xieguaiwu <xieguaiwu@163.com>
2026-01-11 15:54:34 +0800

    Add files via upload

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,16 +1,20 @@
 require("full-border"):setup()
 function Linemode:size_and_mtime()
-	local year = os.date("%Y")
-	local time = (self._file.cha.mtime or 0) // 1
-
-	if time > 0 and os.date("%Y", time) == year then
-		time = os.date("%b %d %H:%M", time)
-	else
-		time = time and os.date("%b %d  %Y", time) or ""
-	end
-
-	local size = self._file:size()
-	return ui.Line(string.format(" %s %s ", size and ya.readable_size(size) or "-", time))
+    local year = os.date("%Y")
+    local mtime = self._file.cha.mtime
+    local time_str = ""
+    
+    if mtime and mtime > 0 then
+        local t = mtime // 1
+        if os.date("%Y", t) == year then
+            time_str = os.date("%b %d %H:%M", t)
+        else
+            time_str = os.date("%b %d  %Y", t)
+        end
+    end
+    
+    local size = self._file:size()
+    return ui.Line(string.format(" %s %s ", size and ya.readable_size(size) or "-", time_str))
 end
 
 Status:children_add(function(self)
