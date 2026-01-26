7f2e9e839ac165dd65a4725232f3f2a1f8b7835e
xieguaiwu <xieguaiwu@163.com>
2026-01-26 11:01:43 +0800

    Update setup_day.sh

--- scripts/setup_day.sh
+++ scripts/setup_day.sh
@@ -9,7 +9,7 @@ sudo passwd root
 echo ">>> 更新系统软件包..."
 sudo dnf upgrade --refresh -y
 sudo dnf install -y curl git npm wget gawk node yacc fastfetch pinta torbrowser-launcher
-sudo dnf install -y clash-verge flatpak shotcut eyeD3 exiftool qpdf
+sudo dnf install -y clash-verge flatpak shotcut eyeD3 exiftool qpdf chromium
 sudo dnf install -y fcitx5 fcitx5-chinese-addons fcitx5-configtool fcitx5-qt fcitx5-gtk xorg-x11-font-utils cabextract
 
 echo ">>> Installing fish shell and wezterm..."
