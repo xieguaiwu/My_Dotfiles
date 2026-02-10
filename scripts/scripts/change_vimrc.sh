94f5238e3b4b4492c3b90dd73ea79ec4f3e01efe
xieguaiwu <xieguaiwu@163.com>
2026-02-10 22:03:33 +0800

    Delete scripts/scripts directory

--- scripts/scripts/change_vimrc.sh
+++
@@ -1,6 +0,0 @@
-set -e
-cd ~
-git clone https://github.com/xieguaiwu/My_Vim
-mv ~/My_Vim/vimrc ~/.vimrc
-vim ~/.vimrc
-vim -c "so" -c "q"
--- scripts/scripts/setup_charging_threshold.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-
-sudo nano /sys/class/power_supply/BAT*/charge_control_end_threshold
