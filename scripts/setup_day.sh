4722c4007c13ce762745d7e893b1e37cf4e5a234
xieguaiwu <xieguaiwu@163.com>
2026-02-15 12:01:45 +0800

    Update setup_day.sh

--- scripts/setup_day.sh
+++ scripts/setup_day.sh
@@ -7,7 +7,7 @@ echo ">>> You can use 'sudo passwd root' to set the root password..."
 echo ">>> 更新系统软件包..."
 sudo dnf upgrade --refresh -y
 sudo dnf install -y curl git npm wget gawk node yacc fastfetch pinta torbrowser-launcher
-sudo dnf install -y clash-verge flatpak shotcut eyeD3 exiftool qpdf chromium cava
+sudo dnf install -y clash-verge flatpak shotcut eyeD3 exiftool qpdf chromium cava openssh-server
 sudo dnf install -y fcitx5 fcitx5-chinese-addons fcitx5-configtool fcitx5-qt fcitx5-gtk xorg-x11-font-utils cabextract
 
 cd ~/Downloads
