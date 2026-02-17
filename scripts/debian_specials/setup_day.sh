a5179bbabfa53cc1e6460484e112ff76aa41c559
xieguaiwu <xieguaiwu@163.com>
2026-02-17 12:36:31 +0800

    Update setup_day.sh

--- scripts/debian_specials/setup_day.sh
+++ scripts/debian_specials/setup_day.sh
@@ -24,6 +24,10 @@ mv ./vim-plug/plug.vim ~/.vim/autoload
 echo ">>> Installing fish shell and wezterm..."
 sudo apt install -y fish
 
+cd ~/Downloads
+wget "https://github.com/sheeki03/tirith/releases/download/v0.1.9/tirith_0.1.9_amd64.deb"
+sudo dnf -y install ./tirith_0.1.9_amd64.deb
+
 curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
 echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
 sudo chmod 644 /usr/share/keyrings/wezterm-fury.gpg
