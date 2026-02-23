1f52b4300dd8f42960f3bad3b26f9761d05acccc
xieguaiwu <xieguaiwu@163.com>
2026-02-23 20:04:49 +0800

    Update setup_dev.sh

--- scripts/setup_dev.sh
+++ scripts/setup_dev.sh
@@ -10,7 +10,9 @@ echo ">>> 安装 Python & Haskell 开发环境..."
 sudo dnf install -y python3 python3-pip python3-virtualenv python3-devel autopep8 ghc
 pip3 install --upgrade pip ipython black flake8 mypy httplib2 redis tbomb
 pip3 install pynvim pyinstaller numpy pandas phone thread threadpool
+echo ">>> 现在开始安装Haskell,请记得勾选下载Haskell的language-server..."
 curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
+stack install stylish-haskell
 
 echo ">>> 安装 Java "
 sudo dnf install -y java-latest-openjdk java-latest-openjdk-devel maven gradle
