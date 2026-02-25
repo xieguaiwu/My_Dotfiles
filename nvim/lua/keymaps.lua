596d679d2e96dcaba5144229acc542e7aacca9da
xieguaiwu <xieguaiwu@163.com>
2026-02-25 18:18:30 +0800

    Add files via upload

---
+++ nvim/lua/keymaps.lua
@@ -0,0 +1,36 @@
+-- ~/.config/nvim/lua/keymaps.lua
+
+local map = vim.keymap.set
+
+-- jump from lines
+map("n", "j", "gj")
+map("n", "k", "gk")
+
+-- copy to system clipboard
+map("n", "9y", "\"+y")
+
+-- quick replacing
+map("n", "<C-h>", ":%s//g<Left><Left>", { noremap = true, silent = false, desc = "Quick Replace" })
+
+-- move lines up and down
+map("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
+map("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
+map("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+map("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+
+-- muti-window
+map("n", "fl", ":set splitright<CR>:vsplit<CR>", { desc = "Vertical Split Right" })
+map("n", "fh", ":set nosplitright<CR>:vsplit<CR>", { desc = "Vertical Split Left" })
+map("n", "fk", ":set nosplitbelow<CR>:split<CR>", { desc = "Horizontal Split Up" })
+map("n", "fj", ":set splitbelow<CR>:split<CR>", { desc = "Horizontal Split Down" })
+
+-- for tab pages in vim
+map("n", "<A-t>", ":tabnew<CR>", { desc = "New Tab" })
+map("n", "<C-l>", ":tabnext<CR>", { desc = "Next Tab" })
+map("n", "<C-k>", ":tabprevious<CR>", { desc = "Previous Tab" })
+
+local utils = require("utils")
+
+-- 编译/运行
+map("n", "<C-A-b>", utils.CompileRun, { desc = "Compile and Run File" })
+map("n", "<C-A-f>", utils.SelfFormat, { desc = "Formatting the File" })
---
+++ nvim/lua/utils.lua
@@ -0,0 +1,88 @@
+-- ~/.config/nvim/lua/utils.lua
+
+local M = {}
+
+M.CompileRun = function()
+    vim.cmd("w") -- 写入文件
+    local file = vim.fn.expand('%')
+    local base = vim.fn.expand('%<')
+
+    if not vim.fn.filereadable(file) then
+        print("Error: File not readable - " .. file)
+        return
+    end
+
+    local ft = vim.bo.filetype
+    local cmd = nil
+
+    if ft == 'cpp' or ft == 'cc' then
+        cmd = string.format("!g++ -std=c++17 -O2 -Wall %s -o %s", file, base)
+    elseif ft == 'c' then
+        cmd = string.format("!gcc -std=c11 -O2 -Wall %s -o %s", file, base)
+    elseif ft == 'java' then
+        local classname = vim.fn.fnamemodify(file, ':t:r')
+        cmd = string.format("!javac -d . %s && java -cp . %s", file, classname)
+    elseif ft == 'python' then
+        cmd = string.format("!python3 %s", file)
+    elseif ft == 'sh' then
+        cmd = string.format("!sh %s", file)
+    elseif ft == 'rust' then
+        cmd = ":RustBuild | RustRun"
+    elseif ft == 'go' then
+        cmd = string.format("!go build %s", file)
+    elseif ft == 'haskell' then
+        cmd = string.format("!ghc -O2 -Wall -threaded -rtsopts -with-rtsopts=-N -o %s %s", base, file)
+    elseif ft == 'asm' or ft == 'nasm' then
+        local obj_file = base .. ".o"
+        local exe_file = base
+        local asm_arch = vim.fn.input("Enter architecture (32/64) [64]: ", "64")
+        local asm_format = "elf64"
+
+        if asm_arch == "32" then
+            asm_format = "elf32"
+        end
+
+        -- 先编译为对象文件，然后链接
+        cmd = string.format("!nasm -f %s -g %s -o %s && ld -m %s -o %s %s",
+            asm_format, file, obj_file,
+            (asm_arch == "32" and "elf_i386" or "elf_x86_64"),
+            exe_file, obj_file)
+    end
+
+    if cmd then
+        vim.cmd(cmd)
+    end
+end
+
+M.SelfFormat = function()
+    local ft = vim.bo.filetype
+    local format_command = nil
+
+    if ft == 'c' or ft == 'cpp' or ft == 'cc' or ft == 'java' then
+        format_command = string.format("!astyle --mode=c --style=java --indent=tab --pad-oper --pad-header --unpad-paren --suffix=none %s", vim.fn.expand('%'))
+    elseif ft == 'rust' then
+        format_command = string.format("!rustfmt %s", vim.fn.expand('%'))
+    elseif ft == 'go' then
+        format_command = string.format("!gofmt -w %s", vim.fn.expand('%'))
+    elseif ft == 'python' then
+        format_command = string.format("!autopep8 --hang-closing --aggressive --in-place %s", vim.fn.expand('%'))
+    elseif ft == 'asm' or ft == 'nasm' or ft == 'gas' or ft == 's' then
+        -- 汇编代码格式化：使用indent或自定义脚本
+        format_command = string.format("!cat %s | expand -t 8 | unexpand -t 8 > %s.tmp && mv %s.tmp %s",
+            vim.fn.expand('%'), vim.fn.expand('%'),
+            vim.fn.expand('%'), vim.fn.expand('%'))
+    elseif ft == 'haskell' then
+        local config = vim.fn.stdpath("config") .. "/stylish-haskell/config.yaml" --使用~/.config/ 来作为formatter的地址，而非~/
+        format_command = string.format("!stylish-haskell -c %s -i %s", config, vim.fn.expand('%'))
+    else
+        format_command = "Autoformat"
+    end
+
+    if format_command ~= nil then
+        vim.cmd(":w")
+        vim.cmd(format_command)
+    end
+
+end
+
+return M
