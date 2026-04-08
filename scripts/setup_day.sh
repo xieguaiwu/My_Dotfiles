3b370e851e554e8e907873d80f4e56e3132cfe64
xieguaiwu <xieguaiwu@163.com>
2026-04-08 09:05:27 +0800

    Update setup_day.sh

--- scripts/setup_day.sh
+++ scripts/setup_day.sh
@@ -20,6 +20,7 @@ mv ~/vim-plug/plug.vim ~/.vim/autoload
 sudo npm i -g @iflow-ai/iflow-cli
 sudo npm install -g opencode-ai
 sudo npm install -g oh-my-opencode@latest
+sudo npm i -g opencode-ai@latest 
 
 echo ">>> Installing fish shell and wezterm..."
 sudo dnf install -y fish
